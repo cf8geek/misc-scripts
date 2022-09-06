5357b00dc20815e9e1ed84d0385155097711931e
cf8geek <cf8geek@users.noreply.github.com>
2022-09-06 16:55:36 -0500

    Update un-safelinks.sh

--- un-safelinks/un-safelinks.sh
+++ un-safelinks/un-safelinks.sh
@@ -2,5 +2,5 @@
 # usage: ./un-safelinks.sh 'https://protection-bla-blah-bla/url-endoded-blah-bla-blah'
 echo
 echo
-echo $1 | sed 's/\&data.*//' | sed 's/https:\/\/nam02.safelinks.protection.outlook.com\/?url=//' | sed 's/%2F/\//g' | sed 's/%3A/:/g' | sed 's/%3F/?/g' | sed 's/%3D/=/g' | sed 's/%25/%/g' 
+echo $1 | sed 's/\&data.*//' | sed 's/https:\/\/nam[0-9]\{2\}.safelinks.protection.outlook.com\/?url=//' | sed 's/%2F/\//g' | sed 's/%3A/:/g' | sed 's/%3F/?/g' | sed 's/%3D/=/g' | sed 's/%25/%/g' 
 echo
