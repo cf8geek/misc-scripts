a4210e1b610541d1e5a931f1bfe197ac750d1531
cf8geek <cf8geek@users.noreply.github.com>
2022-11-29 14:29:44 -0600

    Create factory-reset.sh

---
+++ yubikey-stuff/factory-reset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://support.yubico.com/hc/en-us/articles/360013757959-Resetting-Your-YubiKey-5-Series-to-Factory-Defaults
+## https://support.yubico.com/hc/en-us/articles/360013647680-Resetting-the-OTP-Applet-on-the-YubiKey
+## https://support.yubico.com/hc/en-us/articles/360016648899-Resetting-the-FIDO2-Application-on-Your-YubiKey-or-Security-Key
+## https://support.yubico.com/hc/en-us/articles/360013645480-Resetting-the-Smart-Card-PIV-Applet-on-Your-YubiKey
+## https://support.yubico.com/hc/en-us/articles/360013712719-Resetting-the-OATH-Applet-on-the-YubiKey
+## https://support.yubico.com/hc/en-us/articles/360013761339-Resetting-the-OpenPGP-Applet-on-the-YubiKey
