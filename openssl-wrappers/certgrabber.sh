2b247e8416703cba5596d42a91e48a5372821ab6
cf8geek <cf8geek@users.noreply.github.com>
2023-08-03 12:00:47 -0500

    Create certgrabber.sh

---
+++ openssl-wrappers/certgrabber.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#stolen from:
+#https://stackoverflow.com/questions/7885785/using-openssl-to-get-the-certificate-from-a-server/
+if [ -z "$1" ]
+then
+	echo "need to pass FQDN"
+	echo "PROTIP: grep for 'After' to see expiry"
+else
+	echo | \
+		openssl s_client -servername $1 -connect $1:443 2>/dev/null | \
+		openssl x509 -text
+fi
