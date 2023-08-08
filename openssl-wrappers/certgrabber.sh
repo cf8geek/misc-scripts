6088114ac790b28e1a01b272551cfdbc360dc519
cf8geek <cf8geek@users.noreply.github.com>
2023-08-08 16:26:26 -0500

    Update certgrabber.sh

--- openssl-wrappers/certgrabber.sh
+++ openssl-wrappers/certgrabber.sh
@@ -8,5 +8,6 @@ then
 else
 	echo | \
 		openssl s_client -servername $1 -connect $1:443 2>/dev/null | \
-		openssl x509 -text
+		openssl x509 -text | \
+  			grep -e Issuer: -e After -e Before -e Subject:
 fi
