ff985b4ac2d24e64f3ecc6b2cc622c4f85539817
cf8geek <cf8geek@users.noreply.github.com>
2025-12-19 17:10:47 -0600

    Update screenshot-organization_full-send.sh

--- monthly-subfolders/screenshot-organization_full-send.sh
+++ monthly-subfolders/screenshot-organization_full-send.sh
@@ -17,16 +17,16 @@ mkdir $year/09-September
 mkdir $year/10-October
 mkdir $year/11-November
 mkdir $year/12-December
-mv "Screenshot From $year-01-*.png" ./$year/01-January/
-mv "Screenshot From $year-02-*.png" ./$year/02-February/
-mv "Screenshot From $year-03-*.png" ./$year/03-March/
-mv "Screenshot From $year-04-*.png" ./$year/04-April/
-mv "Screenshot From $year-05-*.png" ./$year/05-May/
-mv "Screenshot From $year-06-*.png" ./$year/06-June/
-mv "Screenshot From $year-07-*.png" ./$year/07-July/
-mv "Screenshot From $year-08-*.png" ./$year/08-August/
-mv "Screenshot From $year-09-*.png" ./$year/09-September/
-mv "Screenshot From $year-10-*.png" ./$year/10-October/
-mv "Screenshot From $year-11-*.png" ./$year/11-November/
-mv "Screenshot From $year-12-*.png" ./$year/12-December/
+mv "Screenshot From $year-01-"*".png" ./$year/01-January/
+mv "Screenshot From $year-02-"*".png" ./$year/02-February/
+mv "Screenshot From $year-03-"*".png" ./$year/03-March/
+mv "Screenshot From $year-04-"*".png" ./$year/04-April/
+mv "Screenshot From $year-05-"*".png" ./$year/05-May/
+mv "Screenshot From $year-06-"*".png" ./$year/06-June/
+mv "Screenshot From $year-07-"*".png" ./$year/07-July/
+mv "Screenshot From $year-08-"*".png" ./$year/08-August/
+mv "Screenshot From $year-09-"*".png" ./$year/09-September/
+mv "Screenshot From $year-10-"*".png" ./$year/10-October/
+mv "Screenshot From $year-11-"*".png" ./$year/11-November/
+mv "Screenshot From $year-12-"*".png" ./$year/12-December/
 exit
